b766a6de18693364e3663b5c9b1f0c185682bd41
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-02 01:02:14 +0200

    add sentry

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import express from "express";
 import * as dotevnv from "dotenv";
 import cors from "cors";
@@ -16,6 +18,18 @@ if (!process.env.PORT) {
 
 const PORT = parseInt(process.env.PORT as string, 10);
 
+Sentry.init({
+  dsn: "https://6e275a4fb52b9ca6e44220b4c39dc16b@o4507378463080448.ingest.de.sentry.io/4507529028042832",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 const app = express();
 
 app.use(express.json());
@@ -23,7 +37,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
-app.use(errorHandler);
+Sentry.setupExpressErrorHandler(app);
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 app.use("/", authRouter, customerRouter, orderRouter, productRouter);
 
